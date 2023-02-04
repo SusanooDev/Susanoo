7b71d095fc4419a1714e2bee8e1cb7176809ee49
Miraide Augusto Bassam [SNT DDE/ERC/PPC] <miraide16.22@gmail.com>
2023-02-04 20:41:56 +0000

    Ajout des fichiers ajoutProduit.php and modifProduit.php v4

--- adjout_produit.php
+++ adjout_produit.php
@@ -6,6 +6,7 @@ $nom=$_POST['nom'];
 $prix=$_POST['prix'];
 $stock=$_POST['stock'];
 $description=$_POST['description'];
+//Insertion dans la base de donnees
 $sql_adjout="insert into produits values(null, '$nom', '$prix', '$stock', '$description')";
 $query=mysqli_query($connect, $sql_adjout);
 header("location:liste_produit.php");
